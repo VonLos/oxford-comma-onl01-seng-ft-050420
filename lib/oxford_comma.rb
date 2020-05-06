05a0c50f0d92affee0aac25672797acaad8b6e42
rylandgrounds <rylandgrounds@gmail.com>
2020-05-06 14:32:28 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
+require "pry"
 def oxford_comma(array)
-
+   
+    if array.length > 2
+        array[-1].insert(0,"and ")
+        array.join(", ")
+    elsif array.length == 2
+       array.join(" and ")
+    else
+        array[0]
+    end
 end
+
